670f8b620d806aaa0dcf30ab2a8caf8a60eedb4a
Clément Dumas <butanium.contact@gmail.com>
2022-12-02 13:23:40 +0100

    reformat

--- parsing/tast.ml
+++ parsing/tast.ml
@@ -1,54 +1,48 @@
-
 (* arbres issus du typeur *)
 
 type unop = Ast.unop
-
 type binop = Ast.binop
-
 type constant = Ast.constant
-
 type incdec = Ast.incdec
 
-type function_ = {
-    fn_name: string;
-  fn_params: var list;
-     fn_typ: typ list;
-}
+type function_ = { fn_name : string; fn_params : var list; fn_typ : typ list }
 
 and field = {
-         f_name: string;
-          f_typ: typ;
-  mutable f_ofs: int; (* relatif à l'adresse de l'objet *)
+  f_name : string;
+  f_typ : typ;
+  mutable f_ofs : int; (* relatif à l'adresse de l'objet *)
 }
 
 and structure = {
-          s_name: string;
-        s_fields: (string, field) Hashtbl.t;
-  mutable s_size: int; (* taille calculee en octets *)
+  s_name : string;
+  s_fields : (string, field) Hashtbl.t;
+  mutable s_size : int; (* taille calculee en octets *)
 }
 
 and typ =
-  | Tint | Tbool | Tstring
+  | Tint
+  | Tbool
+  | Tstring
   | Tstruct of structure
   | Tptr of typ
   | Twild (* type wildcard, tout type *)
-  | Tmany of typ list (* 0 pour type retour instructions et >=2 pour retour functions *)
-  (* TODO autres types pour l'analyse semantique, si besoin *)
+  | Tmany of typ list
+(* 0 pour type retour instructions et >=2 pour retour functions *)
+(* TODO autres types pour l'analyse semantique, si besoin *)
 
 and var = {
-          v_name: string;
-            v_id: int;  (* unique *)
-           v_loc: Ast.location;
-           v_typ: typ;
-         v_depth: int;  (* index de portee *)
-  mutable v_used: bool;
-  mutable v_addr: int;  (* adresse relative au pointer de frame (rbp) *)
-  (* TODO autres informations pour la production de code, si besoin *)
+  v_name : string;
+  v_id : int; (* unique *)
+  v_loc : Ast.location;
+  v_typ : typ;
+  v_depth : int; (* index de portee *)
+  mutable v_used : bool;
+  mutable v_addr : int;
+      (* adresse relative au pointer de frame (rbp) *)
+      (* TODO autres informations pour la production de code, si besoin *)
 }
 
-and expr =
-  { expr_desc: expr_desc;
-    expr_type : typ; }
+and expr = { expr_desc : expr_desc; expr_type : typ }
 
 and expr_desc =
   | TEskip
@@ -69,8 +63,5 @@ and expr_desc =
   | TEprint of expr list
   | TEincdec of expr * incdec
 
-type tdecl =
-  | TDfunction of function_ * expr
-  | TDstruct of structure
-
+type tdecl = TDfunction of function_ * expr | TDstruct of structure
 type tfile = tdecl list
