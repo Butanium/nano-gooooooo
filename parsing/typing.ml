a5c0d3838a105eb203180d946b003c47c8ff608a
Clément Dumas <butanium.contact@gmail.com>
2022-12-03 16:20:58 +0100

    finished 2 first phases

--- parsing/typing.ml
+++ parsing/typing.ml
@@ -3,15 +3,15 @@ open Lib
 open Ast
 open Tast
 
+type boolean = bool
+
 let debug = ref false
 let dummy_loc = (Lexing.dummy_pos, Lexing.dummy_pos)
 
 exception Error of Ast.location * string
-exception ErrorExpr of Ast.location * string * pexpr_desc
 exception Anomaly of string
 
 let error loc e = raise (Error (loc, e))
-let error_expr loc e expr = raise (ErrorExpr (loc, e, expr))
 (* TODO environnement pour les types structure *)
 
 (* TODO environnement pour les fonctions *)
@@ -31,6 +31,8 @@ let throw_expected_type loc ~expected typ =
     (Printf.sprintf "expected type %s, got %s instead" (string_of_type expected)
        (string_of_type typ))
 
+let throw_undeclared loc k = error loc (Printf.sprintf "undeclared name: %s" k)
+
 let check_type loc ~expected typ =
   if expected <> typ then throw_expected_type loc ~expected typ
 
@@ -40,8 +42,10 @@ module Henv = struct
   type ('a, 'b) hashtable = ('a, 'b) t
   type 'b t = { dummy_init : string -> 'b; henv : (string, 'b) hashtable }
 
+  let mem env = mem env.henv
+
   let add loc env name value =
-    if mem env.henv name then
+    if mem env name then
       error loc (Printf.sprintf "%s has been declared twice " name);
     add env.henv name value
 
@@ -51,18 +55,18 @@ module Henv = struct
   let find env = find env.henv
   let find_opt env = find_opt env.henv
 
-  let find_exn loc env k =
-    match find_opt env k with
-    | None -> error loc (Printf.sprintf "undeclared name: %s" k)
-    | Some v -> v
+  let find_exn ~loc env k =
+    match find_opt env k with None -> throw_undeclared loc k | Some v -> v
 end
 
 let rec type_type env_struct = function
+  | PTident { id; _ } when Henv.mem env_struct id ->
+      Tstruct (Henv.find env_struct id)
   | PTident { id = "int"; _ } -> Tint
   | PTident { id = "bool"; _ } -> Tbool
   | PTident { id = "string"; _ } -> Tstring
-  | PTident { id; loc } -> Tstruct (Henv.find_exn loc env_struct id)
   | PTptr ty -> Tptr (type_type env_struct ty)
+  | PTident { id; loc } -> throw_undeclared loc id
 (* | _ -> error dummy_loc "unknown struct " *)
 (* TODO type structure *)
 
@@ -99,12 +103,16 @@ module Env = struct
 
   let empty = M.empty
   let find = M.find
+
+  let find_exn loc env k =
+    match M.find_opt k env with Some r -> r | None -> throw_undeclared loc k
+
   let add env v = M.add v.v_name v env
   let all_vars = ref []
 
   let check_unused () =
     let check v =
-      if v.v_name <> "_" && (* TODO used *) true then
+      if v.v_name <> "_" && (* TODO not used *) false then
         error v.v_loc "unused variable"
     in
     List.iter check !all_vars
@@ -140,76 +148,76 @@ let get_unop_type loc unop expr =
       | _ -> throw_expected_type loc ~expected:(Tptr Twild) expr.expr_type)
   | Uamp -> Tptr expr.expr_type
 
-let rec type_expr env e =
-  let e, ty, rt = expr_desc env e.pexpr_loc e.pexpr_desc in
-  ({ expr_desc = e; expr_type = ty }, rt)
+let type_function_body (structs : structure Henv.t) funs return_types =
+  let rec type_expr env e =
+    let e, ty, rt = expr_desc env e.pexpr_loc e.pexpr_desc in
+    ({ expr_desc = e; expr_type = ty }, rt)
+  (*  returns the expr, the type of the expr and if there will be a return statement in any case *)
+  and expr_desc env loc expr_p =
+    match expr_p with
+    | PEskip -> (TEskip, tvoid, false)
+    | PEconstant c -> (TEconstant c, type_of_const c, false)
+    | PEbinop (op, e1, e2) ->
+        let ({ expr_type = typ1; _ } as e1), _ = type_expr env e1 in
+        let ({ expr_type = typ2; _ } as e2), _ = type_expr env e2 in
+        if typ1 = typ2 && typ1 = Tint then (TEbinop (op, e1, e2), Tint, false)
+        else
+          error loc
+            (Printf.sprintf
+               "type error in binary operation, expected type Int, got %s and \
+                %s"
+               (string_of_type typ1) (string_of_type typ2))
+    | PEunop (op, e1) ->
+        let expr, rt = type_expr env e1 in
+        (TEunop (op, expr), get_unop_type loc op expr, false)
+    | PEcall ({ id = "fmt.Print"; _ }, el) ->
+        let el = List.map (fun e -> type_expr env e |> fst) el in
+        fmt_used := true;
+        if not !fmt_imported then
+          error loc "package fmt is used but not imported";
+        (TEprint el, tvoid, false)
+    | PEcall ({ id = "new"; _ }, [ { pexpr_desc = PEident { id; _ }; _ } ]) ->
+        let ty =
+          match id with
+          | id when Henv.mem structs id -> Tstruct (Henv.find structs id)
+          | "int" -> Tint
+          | "bool" -> Tbool
+          | "string" -> Tstring
+          | id -> throw_undeclared loc id
+        in
+        (TEnew ty, Tptr ty, false)
+        (* declaration d'un pointeur ty *)
+    | PEcall ({ id = "new"; _ }, _) -> error loc "new expects a type"
+    | PEcall (id, el) -> (* TODO *) assert false
+    | PEfor (e, b) ->
+        let b, _ = type_expr env b in
+        check_type loc ~expected:Tbool b.expr_type;
+        let e, _ = type_expr env e in
+        (TEfor (e, b), tvoid, false)
+    | PEif (e1, e2, e3) ->
+        let f = type_expr env in
+        let (e1, _), (e2, rtThen), (e3, rtElse) = (f e1, f e2, f e3) in
+        check_type loc ~expected:Tbool e1.expr_type;
+        (TEif (e1, e2, e3), tvoid, rtThen && rtElse)
+    | PEnil -> (TEnil, tvoid, false)
+    | PEident { id; _ } -> (
+        (* TODO *)
+        try
+          let v = Env.find id env in
+          (TEident v, v.v_typ, false)
+        with Not_found -> error loc ("unbound variable " ^ id))
+    | PEdot (e, id) -> (* TODO *) assert false
+    | PEassign (lvl, el) -> (* TODO *) (TEassign ([], []), tvoid, false)
+    | PEreturn el -> (* TODO *) (TEreturn [], tvoid, true)
+    | PEblock el -> (* TODO *) (TEblock [], tvoid, false)
+    | PEincdec (e, op) ->
+        let e, fmt = type_expr env e in
+        check_type loc ~expected:Tint e.expr_type;
+        (TEincdec (e, op), tvoid, fmt)
+    | PEvars _ -> (* TODO *) assert false
+  in
+  type_expr Env.empty
 
-(**  returns the expr, the type of the expr and if a format import is needed
-*)
-and expr_desc env loc expr_p =
-  match expr_p with
-  | PEskip -> (TEskip, tvoid, false)
-  | PEconstant c -> (TEconstant c, type_of_const c, false)
-  | PEbinop (op, e1, e2) ->
-      let ({ expr_type = typ1; _ } as e1), need_f1 = type_expr env e1 in
-      let ({ expr_type = typ2; _ } as e2), need_f2 = type_expr env e2 in
-      if typ1 = typ2 && typ1 = Tint then
-        (TEbinop (op, e1, e2), Tint, need_f1 || need_f2)
-      else
-        error_expr loc
-          (Printf.sprintf
-             "type error in binary operation, expected type Int, got %s and %s"
-             (string_of_type typ1) (string_of_type typ2))
-          expr_p
-  | PEunop (op, e1) ->
-      let expr, fmt = type_expr env e1 in
-      (TEunop (op, expr), get_unop_type loc op expr, fmt)
-  | PEcall ({ id = "fmt.Print"; _ }, el) ->
-      let el = List.map (fun e -> type_expr env e |> fst) el in
-      (TEprint el, tvoid, true)
-      (* TODO : check si je suis sensé mettre true ou false *)
-  | PEcall ({ id = "new"; _ }, [ { pexpr_desc = PEident { id; _ }; _ } ]) ->
-      let ty =
-        match id with
-        | "int" -> Tint
-        | "bool" -> Tbool
-        | "string" -> Tstring
-        | _ ->
-            (* TODO que faire si c'est un type custom *)
-            error loc ("no such type " ^ id)
-      in
-      (TEnew ty, Tptr ty, false)
-      (* declaration d'un pointeur ty *)
-  | PEcall ({ id = "new"; _ }, _) -> error loc "new expects a type"
-  | PEcall (id, el) -> (* TODO *) assert false
-  | PEfor (e, b) ->
-      let b, fmt = type_expr env b in
-      check_type loc ~expected:Tbool b.expr_type;
-      let e, fmt2 = type_expr env e in
-      (TEfor (e, b), tvoid, fmt || fmt2)
-  | PEif (e1, e2, e3) ->
-      let f = type_expr env in
-      let (e1, fmt1), (e2, fmt2), (e3, fmt3) = (f e1, f e2, f e3) in
-      check_type loc ~expected:Tbool e1.expr_type;
-      (TEif (e1, e2, e3), tvoid, fmt1 || fmt2 || fmt3)
-  | PEnil -> (TEnil, tvoid, false)
-  | PEident { id; _ } -> (
-      (* TODO *)
-      try
-        let v = Env.find id env in
-        (TEident v, v.v_typ, false)
-      with Not_found -> error loc ("unbound variable " ^ id))
-  | PEdot (e, id) -> (* TODO *) assert false
-  | PEassign (lvl, el) -> (* TODO *) (TEassign ([], []), tvoid, false)
-  | PEreturn el -> (* TODO *) (TEreturn [], tvoid, true)
-  | PEblock el -> (* TODO *) (TEblock [], tvoid, false)
-  | PEincdec (e, op) ->
-      let e, fmt = type_expr env e in
-      check_type loc ~expected:Tint e.expr_type;
-      (TEincdec (e, op), tvoid, fmt)
-  | PEvars _ -> (* TODO *) assert false
-
-let found_main = ref false
 let dummy_function _ = assert false
 
 let dummy_structure s_name =
@@ -279,27 +287,44 @@ let phase2 structs funs = function
         ps_fields
 
 (* 3. type check function bodies and compute size and offsets for sturct, also compute v_addr *)
-let decl = function
-  | PDfunction
-      { pf_name = { id; loc; _ }; pf_body = e; pf_return_types = tyl; _ } ->
+let decl (structs : structure Henv.t) (functions : function_ Henv.t) = function
+  | PDfunction { pf_name = { id; loc }; pf_body; pf_return_types; _ } ->
       (* TODO check name and type *)
-      let f = { fn_name = id; fn_params = []; return_types = [] } in
-      let e, rt = type_expr Env.empty e in
-      TDfunction (f, e)
+      let e, rt =
+        type_function_body structs functions pf_return_types pf_body
+      in
+      if not rt then
+        error loc (Printf.sprintf "No return statement in function %s" id);
+      let func = Henv.find functions id in
+      List.fold_left
+        (fun address var ->
+          var.v_addr <- address;
+          address + sizeof var.v_typ)
+        0 func.fn_params
+      |> ignore;
+      TDfunction (func, e)
   | PDstruct { ps_name = { id; _ }; _ } ->
-      (* TODO *)
-      let s = { s_name = id; s_fields = Hashtbl.create 5; s_size = 0 } in
-      TDstruct s
+      let structure = Henv.find structs id in
+      let size =
+        Hashtbl.fold
+          (fun _ field offset ->
+            field.f_ofs <- offset;
+            offset + sizeof field.f_typ)
+          structure.s_fields 0
+      in
+      structure.s_size <- size;
+      TDstruct structure
 
 let file ~debug:b (imp, dl) =
   debug := b;
-  (* fmt_imported := imp; *)
+  fmt_imported := imp;
+  fmt_used := false;
   let struct_env = Henv.create dummy_structure in
   let function_env = Henv.create dummy_function in
   List.iter (phase1 struct_env) dl;
   List.iter (phase2 struct_env function_env) dl;
-  if not !found_main then error dummy_loc "missing method main";
-  let dl = List.map decl dl in
+  Henv.find_exn ~loc:dummy_loc function_env "main" |> ignore;
+  let dl = List.map (decl struct_env function_env) dl in
   Env.check_unused ();
   (* TODO variables non utilisees *)
   if imp && not !fmt_used then error dummy_loc "fmt imported but not used";
