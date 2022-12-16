37b05fcc646811386f6a4e57cfa6345c5df16581
Clément Dumas <butanium.contact@gmail.com>
2022-12-16 12:31:44 +0100

    format code

--- compilation/GoCompile.ml
+++ compilation/GoCompile.ml
@@ -21,9 +21,9 @@
             ...
             calculs
    rsp ---> ...
-
 *)
 [@@@warning "-26"]
+
 (* todo : deleteme *)
 open Format
 open Ast
@@ -33,8 +33,8 @@ open X86_64
 exception Anomaly of string
 
 let debug = ref false
-
 let strings = Hashtbl.create 32
+
 let alloc_string =
   let r = ref 0 in
   fun s ->
@@ -45,17 +45,19 @@ let alloc_string =
 
 let malloc n = movq (imm n) (reg rdi) ++ call "malloc"
 let allocz n = movq (imm n) (reg rdi) ++ call "allocz"
-
 let sizeof = Typing.sizeof
 
 let new_label =
-  let r = ref 0 in fun () -> incr r; "L_" ^ string_of_int !r
+  let r = ref 0 in
+  fun () ->
+    incr r;
+    "L_" ^ string_of_int !r
 
 type env = {
-  exit_label: string;
-  ofs_this: int;
-  nb_locals: int ref; (* maximum *)
-  next_local: int; (* 0, 1, ... *)
+  exit_label : string;
+  ofs_this : int;
+  nb_locals : int ref; (* maximum *)
+  next_local : int; (* 0, 1, ... *)
 }
 
 let empty_env =
@@ -66,103 +68,82 @@ let mk_bool d = { expr_desc = d; expr_type = Tbool }
 (* f reçoit le label correspondant à ``renvoyer vrai'' *)
 let compile_bool f =
   let l_true = new_label () and l_end = new_label () in
-  f l_true ++
-  movq (imm 0) (reg rdi) ++ jmp l_end ++
-  label l_true ++ movq (imm 1) (reg rdi) ++ label l_end
+  f l_true
+  ++ movq (imm 0) (reg rdi)
+  ++ jmp l_end ++ label l_true
+  ++ movq (imm 1) (reg rdi)
+  ++ label l_end
 
-let rec expr env e = match e.expr_desc with
-  | TEskip ->
-    nop
-  | TEconstant (Cbool true) ->
-    movq (imm 1) (reg rdi)
-  | TEconstant (Cbool false) ->
-    movq (imm 0) (reg rdi)
-  | TEconstant (Cint x) ->
-    movq (imm64 x) (reg rdi)
-  | TEnil ->
-    xorq (reg rdi) (reg rdi)
-  | TEconstant (Cstring s) ->
-    (* TODO code pour constante string *) assert false 
-  | TEbinop (Band, e1, e2) ->
-    (* TODO code pour ET logique lazy *) assert false 
-  | TEbinop (Bor, e1, e2) ->
-    (* TODO code pour OU logique lazy *) assert false 
-  | TEbinop (Blt | Ble | Bgt | Bge as op, e1, e2) ->
-    (* TODO code pour comparaison ints *) assert false 
-  | TEbinop (Badd | Bsub | Bmul | Bdiv | Bmod as op, e1, e2) ->
-    (* TODO code pour arithmetique ints *) assert false 
-  | TEbinop (Beq | Bne as op, e1, e2) ->
-    (* TODO code pour egalite toute valeur *) assert false 
-  | TEunop (Uneg, e1) ->
-    (* TODO code pour negation ints *) assert false 
-  | TEunop (Unot, e1) ->
-    (* TODO code pour negation bool *) assert false 
-  | TEunop (Uamp, e1) ->
-    (* TODO code pour & *) assert false 
-  | TEunop (Ustar, e1) ->
-    (* TODO code pour * *) assert false 
-  | TEprint el ->
-    (* TODO code pour Print *) assert false 
-  | TEident x ->
-    (* TODO code pour x *) assert false 
-  | TEassign ([{expr_desc=TEident x}], [e1]) ->
-    (* TODO code pour x := e *) assert false 
-  | TEassign ([lv], [e1]) ->
-    (* TODO code pour x1,... := e1,... *) assert false 
-  | TEassign (_, _) ->
-     assert false
-  | TEblock el ->
-     (* TODO code pour block *) assert false
-  | TEif (e1, e2, e3) ->
-     (* TODO code pour if *) assert false
-  | TEfor (e1, e2) ->
-     (* TODO code pour for *) assert false
-  | TEnew ty ->
-     (* TODO code pour new S *) assert false
-  | TEcall (f, el) ->
-     (* TODO code pour appel fonction *) assert false
-  | TEdot (e1, {f_ofs=ofs}) ->
-     (* TODO code pour e.f *) assert false
-  | TEvars _ ->
-     assert false (* fait dans block *)
-  | TEreturn [] ->
-    (* TODO code pour return e *) assert false
-  | TEreturn [e1] ->
-    (* TODO code pour return e1,... *) assert false
-  | TEreturn _ ->
-     assert false
-  | TEincdec (e1, op) ->
-    (* TODO code pour return e++, e-- *) assert false
+let rec expr env e =
+  match e.expr_desc with
+  | TEskip -> nop
+  | TEconstant (Cbool true) -> movq (imm 1) (reg rdi)
+  | TEconstant (Cbool false) -> movq (imm 0) (reg rdi)
+  | TEconstant (Cint x) -> movq (imm64 x) (reg rdi)
+  | TEnil -> xorq (reg rdi) (reg rdi)
+  | TEconstant (Cstring s) -> (* TODO code pour constante string *) assert false
+  | TEbinop (Band, e1, e2) -> (* TODO code pour ET logique lazy *) assert false
+  | TEbinop (Bor, e1, e2) -> (* TODO code pour OU logique lazy *) assert false
+  | TEbinop (((Blt | Ble | Bgt | Bge) as op), e1, e2) ->
+      (* TODO code pour comparaison ints *) assert false
+  | TEbinop (((Badd | Bsub | Bmul | Bdiv | Bmod) as op), e1, e2) ->
+      (* TODO code pour arithmetique ints *) assert false
+  | TEbinop (((Beq | Bne) as op), e1, e2) ->
+      (* TODO code pour egalite toute valeur *) assert false
+  | TEunop (Uneg, e1) -> (* TODO code pour negation ints *) assert false
+  | TEunop (Unot, e1) -> (* TODO code pour negation bool *) assert false
+  | TEunop (Uamp, e1) -> (* TODO code pour & *) assert false
+  | TEunop (Ustar, e1) -> (* TODO code pour * *) assert false
+  | TEprint el -> (* TODO code pour Print *) assert false
+  | TEident x -> (* TODO code pour x *) assert false
+  | TEassign ([ { expr_desc = TEident x } ], [ e1 ]) ->
+      (* TODO code pour x := e *) assert false
+  | TEassign ([ lv ], [ e1 ]) ->
+      (* TODO code pour x1,... := e1,... *) assert false
+  | TEassign (_, _) -> assert false
+  | TEblock el -> (* TODO code pour block *) assert false
+  | TEif (e1, e2, e3) -> (* TODO code pour if *) assert false
+  | TEfor (e1, e2) -> (* TODO code pour for *) assert false
+  | TEnew ty -> (* TODO code pour new S *) assert false
+  | TEcall (f, el) -> (* TODO code pour appel fonction *) assert false
+  | TEdot (e1, { f_ofs = ofs }) -> (* TODO code pour e.f *) assert false
+  | TEvars _ -> assert false (* fait dans block *)
+  | TEreturn [] -> (* TODO code pour return e *) assert false
+  | TEreturn [ e1 ] -> (* TODO code pour return e1,... *) assert false
+  | TEreturn _ -> assert false
+  | TEincdec (e1, op) -> (* TODO code pour return e++, e-- *) assert false
 
 let function_ f e =
   if !debug then eprintf "function %s:@." f.fn_name;
-  (* TODO code pour fonction *) let s = f.fn_name in label ("F_" ^ s) 
+  (* TODO code pour fonction *)
+  let s = f.fn_name in
+  label ("F_" ^ s)
 
 let decl code = function
   | TDfunction (f, e) -> code ++ function_ f e
   | TDstruct _ -> code
 
-let file ?debug:(b=false) dl =
+let file ?debug:(b = false) dl =
   debug := b;
   (* TODO calcul offset champs *)
-  (* TODO code fonctions *) let funs = List.fold_left decl nop dl in
-  { text =
-      globl "main" ++ label "main" ++
-      call "F_main" ++
-      xorq (reg rax) (reg rax) ++
-      ret ++
-      funs ++
-      inline "
-print_int:
-        movq    %rdi, %rsi
-        movq    $S_int, %rdi
-        xorq    %rax, %rax
-        call    printf
-        ret
-"; (* TODO print pour d'autres valeurs *)
-   (* TODO appel malloc de stdlib *)
+  (* TODO code fonctions *)
+  let funs = List.fold_left decl nop dl in
+  {
+    text =
+      globl "main" ++ label "main" ++ call "F_main"
+      ++ xorq (reg rax) (reg rax)
+      ++ ret ++ funs
+      ++ inline
+           "\n\
+            print_int:\n\
+           \        movq    %rdi, %rsi\n\
+           \        movq    $S_int, %rdi\n\
+           \        xorq    %rax, %rax\n\
+           \        call    printf\n\
+           \        ret\n";
+    (* TODO print pour d'autres valeurs *)
+    (* TODO appel malloc de stdlib *)
     data =
-      label "S_int" ++ string "%ld" ++
-      (Hashtbl.fold (fun l s d -> label l ++ string s ++ d) strings nop)
-    ;
+      label "S_int" ++ string "%ld"
+      ++ Hashtbl.fold (fun l s d -> label l ++ string s ++ d) strings nop;
   }
