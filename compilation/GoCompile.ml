7910f374c197cd207d9d291aaacb93a4f2065fb3
Clément Dumas <butanium.contact@gmail.com>
2023-01-06 17:47:36 +0100

    premier cas de expr

--- compilation/GoCompile.ml
+++ compilation/GoCompile.ml
@@ -22,9 +22,8 @@
             calculs
    rsp ---> ...
 *)
-[@@@warning "-26"]
+[@@@ocamlformat "break-infix = fit-or-vertical"]
 
-(* todo : deleteme *)
 open Format
 open Ast
 open Tast
@@ -63,7 +62,6 @@ let empty_env =
   { exit_label = ""; ofs_this = -1; nb_locals = ref 0; next_local = 0 }
 
 let mk_bool d = { expr_desc = d; expr_type = Tbool }
-
 (* f reçoit le label correspondant à ``renvoyer vrai''
    let compile_bool f =
      let l_true = new_label () and l_end = new_label () in
@@ -73,34 +71,103 @@ let mk_bool d = { expr_desc = d; expr_type = Tbool }
      ++ movq (imm 1) (reg rdi)
      ++ label l_end *)
 
+let comp_to_jump label_true = function
+  | Blt -> jl label_true
+  | Ble -> jle label_true
+  | Bgt -> jg label_true
+  | Bge -> jge label_true
+  | Beq -> je label_true
+  | Bne -> jne label_true
+  | _ -> assert false
+
+let flag_to_rax op =
+  let l_true = new_label () in
+  let l_end = new_label () in
+  comp_to_jump l_true op (* jump to l_true if the condition is true *)
+  ++ movq cfalse !%rax
+     (* if the condition is false then set rax to false and skip the l_true label *)
+  ++ jmp l_end
+  ++ label l_true
+  ++ movq ctrue !%rax
+  ++ label l_end
+
+let int_binop = function
+  | Badd -> addq !%rbx !%rax
+  | Bsub -> subq !%rbx !%rax
+  | Bmul -> imulq !%rbx !%rax
+  | Bdiv -> cqto ++ idivq !%rbx
+  | Bmod -> cqto ++ idivq !%rbx ++ movq !%rdx !%rax
+  | _ -> assert false
+
 let rec expr env e =
   match e.expr_desc with
   | TEskip -> empty_file
-  | TEconstant (Cbool true) -> movq (imm 1) !%rax
+  | TEconstant (Cbool true) -> movq ctrue !%rax
   | TEconstant (Cbool false) -> movq cfalse !%rax
   | TEconstant (Cint x) -> movq (constint64 x) !%rax
   | TEnil -> movq cfalse !%rax
   | TEconstant (Cstring s) ->
       let l = new_string s in
       movq !$l !%rax
+      (* todo check if I need (%rip) *)
   | TEbinop (Band, e1, e2) ->
       let skip = new_label () in
-      expr env e1 ++ (* check if rax is 0 or not *) cmpq cfalse !%rax
-      ++ (* if rax is false then don't evaluate the snd argument *) je skip
-      ++ expr env e2 ++ label skip
+      expr env e1
+      ++ cmpq cfalse !%rax (* check if rax is 0 or not *)
+      ++ je skip (* if rax is false then don't evaluate the snd argument *)
+      ++ expr env e2
+      ++ label skip
   | TEbinop (Bor, e1, e2) ->
       let skip = new_label () in
-      expr env e1 ++ (* check if rax is 0 or not *) cmpq cfalse !%rax
-      ++ (* if rax is true then don't evaluate the snd argument *) jne skip
-      ++ expr env e2 ++ label skip
+      expr env e1
+      ++ cmpq cfalse !%rax (* check if rax is 0 or not *)
+      ++ jne skip (* if rax is true then don't evaluate the snd argument *)
+      ++ expr env e2
+      ++ label skip
   | TEbinop (((Blt | Ble | Bgt | Bge) as op), e1, e2) ->
-      (* TODO code pour comparaison ints *) assert false
+      (* Comparaison ints *)
+      expr env e1
+      ++ pushq !%rax
+      ++ expr env e2
+      ++ popq rbx
+      ++ cmpq !%rax !%rbx
+      ++ flag_to_rax op
   | TEbinop (((Badd | Bsub | Bmul | Bdiv | Bmod) as op), e1, e2) ->
-      (* TODO code pour arithmetique ints *) assert false
+      (* arithmetique ints *)
+      expr env e1 ++ pushq !%rax ++ expr env e2 ++ popq rbx ++ int_binop op
   | TEbinop (((Beq | Bne) as op), e1, e2) ->
-      (* TODO code pour egalite toute valeur *) assert false
-  | TEunop (Uneg, e1) -> (* TODO code pour negation ints *) assert false
-  | TEunop (Unot, e1) -> (* TODO code pour negation bool *) assert false
+      (* egalite toute valeur *)
+      expr env e1
+      ++ (let in_reg =
+            match e1.expr_type with
+            | Tint | Tbool | Tstring | Tptr _ | Tstruct _ -> true
+            | Twild -> raise (Anomaly "wild type in comparaison")
+            | Tmany l -> false
+          in
+          (if in_reg then pushq !%rax else empty_file (* save rax if needed *))
+          ++ expr env e2
+          ++ (if in_reg then popq rbx
+             else empty_file (* pop result if needed *))
+          ++
+          match e1.expr_type with
+          | Tint | Tbool | Tptr _ -> cmpq !%rax !%rbx
+          | Tstring ->
+              movq !%rax !%rdi
+              ++ movq !%rbx !%rsi
+              ++ xorq !%rax !%rax (* set upper 32 bits of rax to 0 *)
+              ++ call "strcmp"
+              ++ cmpq (imm 0) !%rax
+          | Twild -> raise (Anomaly "wild type in comparaison")
+          | Tstruct _ | Tmany _ -> failwith "TODO comp")
+      ++ flag_to_rax op
+      (* result of the comparaison should be in flag, we put it in rax *)
+  | TEunop (Uneg, e1) ->
+      expr env e1 ++ movq (imm 0) !%rbx ++ subq !%rax !%rbx ++ movq !%rbx !%rax
+  | TEunop (Unot, e1) ->
+      expr env e1
+      ++ cmpq cfalse !%rax
+      ++ movq cfalse !%rax
+      ++ sete !%al (* set rax to non 0 if rax is 0 *)
   | TEunop (Uamp, e1) -> (* TODO code pour & *) assert false
   | TEunop (Ustar, e1) -> (* TODO code pour * *) assert false
   | TEprint el -> (* TODO code pour Print *) assert false
@@ -111,9 +178,31 @@ let rec expr env e =
       (* TODO code pour x1,... := e1,... *) assert false
   | TEassign (_, _) -> assert false
   | TEblock el -> (* TODO code pour block *) assert false
-  | TEif (e1, e2, e3) -> (* TODO code pour if *) assert false
-  | TEfor (e1, e2) -> (* TODO code pour for *) assert false
-  | TEnew ty -> (* TODO code pour new S *) assert false
+  | TEif (e1, e2, e3) ->
+      expr e1 env
+      ++ cmpq cfalse !%rax
+      ++
+      let l_false, l_skip = (new_label (), new_label ()) in
+      je l_false
+      ++ expr e2 env
+      ++ jmp l_skip
+      ++ label l_false
+      ++ expr e3 env
+      ++ label l_skip
+  | TEfor (e1, e2) ->
+      let l_start = new_label () in
+      let l_end = new_label () in
+      label l_start
+      ++ expr e1 env
+      ++ cmpq cfalse !%rax
+      ++ je l_end
+      ++ expr e2 env
+      ++ jmp l_start
+      ++ label l_end
+  | TEnew ty ->
+      let size = sizeof ty in
+      malloc (size + 1)
+      (* +1 to allow empty struct *)
   | TEcall (f, el) -> (* TODO code pour appel fonction *) assert false
   | TEdot (e1, { f_ofs = ofs }) -> (* TODO code pour e.f *) assert false
   | TEvars _ -> assert false (* fait dans block *)
@@ -139,7 +228,11 @@ let file ?debug:(b = false) dl =
   let funs = List.fold_left decl empty_file dl in
   {
     text =
-      globl "main" ++ label "main" ++ call "F_main" ++ xorq !%rax !%rax ++ ret
+      globl "main"
+      ++ label "main"
+      ++ call "F_main"
+      ++ xorq !%rax !%rax
+      ++ ret
       ++ funs
       ++ inline
            "\n\
@@ -152,6 +245,7 @@ let file ?debug:(b = false) dl =
     (* TODO print pour d'autres valeurs *)
     (* TODO appel malloc de stdlib *)
     data =
-      label "S_int" ++ string "%ld"
+      label "S_int"
+      ++ string "%ld"
       ++ Hashtbl.fold (fun l s d -> label l ++ string s ++ d) strings empty_file;
   }
