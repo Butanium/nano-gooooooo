af5b6fb5daf9a9ca4d43bbd6535f05a6d6d6d95d
Clément Dumas <butanium.contact@gmail.com>
2023-01-11 13:31:28 +0100

    added error for missuse of ind

--- compilation/x86_64.ml
+++ compilation/x86_64.ml
@@ -92,10 +92,11 @@ let imm i fmt () = fprintf fmt "$%i" i
 let imm32 i fmt () = fprintf fmt "$%ld" i
 let imm64 i fmt () = fprintf fmt "$%Ld" i
 
-let ind ?(ofs = 0) ?index ?(scale = 1) r fmt () =
-  match index with
-  | None -> fprintf fmt "%d(%s)" ofs r
-  | Some r1 -> fprintf fmt "%d(%s,%s,%d)" ofs r r1 scale
+let ind ?(ofs = 0) ?index ?scale r fmt () =
+  match (index, scale) with
+  | None, None -> fprintf fmt "%d(%s)" ofs r
+  | Some r1, Some scale -> fprintf fmt "%d(%s,%s,%d)" ofs r r1 scale
+  | _ -> raise (Invalid_argument "you must specify both index and scale")
 
 let abslab (l : label) fmt () = fprintf fmt "%a" mangle l
 let rellab (l : label) fmt () = fprintf fmt "%a(%%rip)" mangle l
